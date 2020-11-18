62bd0b2b3ac3ccc60a687df2ca61d0418dd538b2
Galaad-CoudercVanhove <galaad-28@orange.fr>
2020-11-18 17:09:39 +0100

    Create TicTacToe.py

---
+++ TicTacToe.py
@@ -0,0 +1,24 @@
+def initialiseGrille(grille):
+    int(compteur)
+    for compteur in range (0,9):
+        grille[compteur] = "_"
+    return grille
+    
+def afficheGrille (grille[9]):
+    int(i)
+    for i in range (0,2):
+        print(grille[3*i],grille[3*i+1],grille[3*i+2],'\n')
+    return grille
+    
+def ajouteSymbole(grille[9],joueur):
+    int(i,j)
+    bool(choixIncorrect)
+    choixIncorrect = True
+    while (choixIncorrect = True):
+        i = int(input("Sur quelle ligne voulez vous jouer ?"))
+        j = int(input("Sur quelle colonne voulez-vouz jouer ?"))
+        if (grille[3*i+j]!= "_"
+            (grille[3*i+j]!= joueur
+            choixIncorrect = False
+    return grille
+    
